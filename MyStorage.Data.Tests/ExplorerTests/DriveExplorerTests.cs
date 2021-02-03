0713656af6694b5e372a0c94e29621f84b3c36d0
mbilal <mbilal5@outlook.com>
2021-02-02 23:01:50 -0500

    Implement test case that ensures a .GetDrives returns non-empty collection of drives.

--- MyStorage.Data.Tests/ExplorerTests/DriveExplorerTests.cs
+++ MyStorage.Data.Tests/ExplorerTests/DriveExplorerTests.cs
@@ -1,14 +1,27 @@
 using System;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using MyStorage.Data.Tests.Fixtures;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace MyStorage.Data.Tests.ExplorerTests
 {
-	public class DriveExplorerTests
+	public class DriveExplorerTests : IClassFixture<GraphServiceClientFixture>
 	{
-		[Fact]
-		public void GetDrivesReturnsNonEmptyCollection()
+		private readonly GraphServiceClient _client;
+		
+		
+		public DriveExplorerTests(GraphServiceClientFixture fixture)
 		{
-			throw new NotImplementedException();
+			_client = fixture.GraphServiceClient;
+		}
+		
+		[Fact]
+		public async Task GetDrivesReturnsNonEmptyCollection()
+		{
+			var drives = await _client.Drives.Request().GetAsync();
+			Assert.True(drives.Count > 0);
 		}
 	}
 }

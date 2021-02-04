e58cfc421491f9c7ee9404778c947b5c7f274990
mbilal <mbilal5@outlook.com>
2021-02-03 22:20:59 -0500

    Add StorageDrive constructor

--- MyStorage.Data/Entities/StorageDrive.cs
+++ MyStorage.Data/Entities/StorageDrive.cs
@@ -1,8 +1,18 @@
+using Microsoft.Graph;
+
 namespace MyStorage.Data.Entities
 {
 	public class StorageDrive
 	{
 		public string Id { get; set; }
 		public string Name { get; set; }
+		
+		public StorageDrive() { }
+		
+		public StorageDrive(Drive drive)
+		{
+			Id = drive.Id;
+			Name = drive.Name;
+		}
 	}
 }

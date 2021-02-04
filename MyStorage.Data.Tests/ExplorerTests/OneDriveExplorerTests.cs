fa61def07591462f024939133dfb0f5a35b3d708
mbilal <mbilal5@outlook.com>
2021-02-03 22:27:44 -0500

    Add test for GetDrive(string id)

--- MyStorage.Data.Tests/ExplorerTests/OneDriveExplorerTests.cs
+++ MyStorage.Data.Tests/ExplorerTests/OneDriveExplorerTests.cs
@@ -26,5 +26,16 @@ namespace MyStorage.Data.Tests.ExplorerTests
 			var drives = await _explorer.GetDrives();
 			Assert.True(drives.Count() > 0);
 		}
+
+		[Fact]
+		public async Task GetDriveByIdReturnsDrive()
+		{
+			// Id of a known OneDrive drive in my developer tenant.
+			string driveId = "b!wI6hmZwqZkS8M88Bsgbp5uwhD_Lv9Y9OiyhZIqo2y9RPgzYZ8aH4Rrh64WbIs-1P";
+			
+			var drive = await _explorer.GetDrive(driveId);
+			
+			Assert.Equal(driveId, drive.Id);
+		}
 	}
 }

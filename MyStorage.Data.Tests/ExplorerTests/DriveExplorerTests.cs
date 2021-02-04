e50cf1cd7cd99a600afcf1bfab63fd4499d9df24
mbilal <mbilal5@outlook.com>
2021-02-03 22:22:57 -0500

    Add OneDriveExplorer

--- MyStorage.Data.Tests/ExplorerTests/DriveExplorerTests.cs
+++ MyStorage.Data.Tests/ExplorerTests/DriveExplorerTests.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Graph;
+using MyStorage.Data.Services.Explorers;
 using MyStorage.Data.Tests.Fixtures;
 using Xunit;
 using Xunit.Abstractions;
@@ -10,18 +12,19 @@ namespace MyStorage.Data.Tests.ExplorerTests
 	public class DriveExplorerTests : IClassFixture<GraphServiceClientFixture>
 	{
 		private readonly GraphServiceClient _client;
-		
-		
+		private readonly OneDriveExplorer _explorer;
+
 		public DriveExplorerTests(GraphServiceClientFixture fixture)
 		{
 			_client = fixture.GraphServiceClient;
+			_explorer = new OneDriveExplorer(_client);
 		}
 		
 		[Fact]
 		public async Task GetDrivesReturnsNonEmptyCollection()
 		{
-			var drives = await _client.Drives.Request().GetAsync();
-			Assert.True(drives.Count > 0);
+			var drives = await _explorer.GetDrives();
+			Assert.True(drives.Count() > 0);
 		}
 	}
 }
